c258954a8c27583f256960e79c7ee40e7201c1ca
ashkankahbasi <92525790+ashkankahbasi@users.noreply.github.com>
2021-11-05 15:03:25 +0330

    Update tictactoe.c
    JUST AN OUTLINE TO HAVE A CLEAR
    LOOK AT HOW TO START WRITING THE
    THE ALGORITHM FOR WINNER AND LOSER.

--- tictactoe.c
+++ tictactoe.c
@@ -167,65 +167,31 @@ void restart(void)
     printf("%s", game);
 }
 
+
 /*
-********Questions before starting the game*********
-Single player or multi player?
-------------------------------------------------------------
-Single player             Multi player
------------------------   ----------------------------------
-Please Enter your name:   Player X, please Enter your names:
-X or O?                   Player O, please Enter your names:
-------------------------------------------------------------
+=======================================
+* JUST AN OUTLINE TO HAVE A CLEAR
+  LOOK AT HOW TO START WRITING THE
+  THE ALGORITHM FOR WINNER AND LOSER.
+=======================================
 
 
+void checkwinner(char c)
+{
+    extern char game[];
+    char x_entries[];
+    char o_entries[];
+    char winner[3][3] = {
+        {'1', '2', '3'},
+        {'4', '5', '6'},
+        {'7', '8', '9'}
+        
+        --> THESE ARE THE WINNERS: 123 - 456 - 789 - 741 - 852 -963 -753 - 951 <--
+    }
 
-********The interface*********
-           |       |
-       1   |   2   |   3
-    _______|_______|_______
-           |       |
-       4   |   5   |   6
-    _______|_______|_______
-           |       |
-       7   |   8   |   9
-           |       |
-
-
-           Statement
-================================
-    (Player name): (Points)
-    (Player name): (Points)
-================================
-    [R]Restart      [X]Exit
-    [L]Records
-
-            - By hitting numbers 1 to 9 They should be replaced by X or O
-            - The player who gets these numbers, wins:
-            123 - 456 - 789 - 741 - 852 -963 -753 - 951
-            - After winning, 1 point adds up to the right player name
-            - The statement part changes to these sentences:
-            It's (Player name)'s turn - (Player name) won - draw
-            - By changing the screens, the game screen save the last state
-            so you can come back to it.
-
-********Questions after hitting "R" or "X"*********
-Are you sure? [Y/N]
-
-
-
-********The records page*********
-========================================
-Name          Points        Date
-------------  ------------  ------------
-Reza Kahba..  65 (Highest)  10/19/2021
-Ashkan        16 (Lowest)   10/19/2021
-========================================
-Name          Points        Date
-------------  ------------  ------------
-Ashkan        16            10/19/2021
-Iman          22            10/19/2021
-Reza Kahba..  65            10/19/2021
-----------------------------------------
-                [B]Back
+    if(c == winner) {
+        return "X won";
+    }
+}
 
 */
